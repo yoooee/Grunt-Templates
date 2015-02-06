62cbcd78413804dcbb0da06d7b4b3553b1ec30f4
Joe Staroba <joe@staroba.com>
2015-02-06 14:17:32 -0600

    Adding base grunt and package files.

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function (grunt) {
+    
+    var config = {};
+
+    grunt.initConfig(config);
+
+    var tasks = [];
+
+    grunt.registerTask('default', tasks);
+};
